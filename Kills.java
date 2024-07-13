b0638ee727851eba55965881620c09e605196223
Stuffy <77872467+stuffyerface@users.noreply.github.com>
2024-07-13 08:15:57 -0400

    Create Kills.java

---
+++ Kills.java
@@ -0,0 +1,70 @@
+List<String> killsKeys = Arrays.asList(
+                // Arcade
+                "Arcade.sw_kills", "Arcade.kills_dragonwars2", "Arcade.kills_throw_out", "Arcade.kills_oneinthequiver", // Is this right?
+                "Arcade.kills_mini_walls", "Arcade.final_kills_mini_walls", "Arcade.rpg_16_kills_party", "Arcade.hunter_kills_farm_hunt",
+                "Arcade.kills_farm_hunt", "Arcade.woolhunt_kills",
+
+                // Arena Brawl
+                "Arena.kills_1v1", "Arena.kills_2v2", "Arena.kills_4v4",
+
+                // Warlords
+                "Battleground.kills",
+
+                // Blitz SG
+                "HungerGames.kills",
+
+                // Cops and Crims TODO: Check tournament kills
+                "MCGO.kills", "MCGO.kills_deathmatch", "MCGO.kills_gungame",
+
+                // Paintball
+                "Paintball.kills",
+
+                // Quakecraft TODO: Double check this
+                "Quake.kills", "Quake.kills_teams",
+
+                // TNT Games
+                "TNTGames.kills_capture", "TNTGames.kills_tntag", "TNTGames.kills_pvprun",
+
+                // UHC
+                "UHC.kills", "UHC.kills_solo",
+
+                // VampireZ
+                "VampireZ.human_kills", "VampireZ.vampire_kills",
+
+                // Walls
+                "Walls.kills",
+
+                // Mega Walls
+                "Walls3.kills",
+
+                // SkyWars
+                "SkyWars.kills",
+
+                // Crazy Walls
+                "TrueCombat.kills",
+
+                // Smash Heroes
+                "SuperSmash.kills",
+
+                // Speed UHC
+                "SpeedUHC.kills_insane", "SpeedUHC.kills_normal",
+
+                // SkyClash
+                "SkyClash.kills",
+
+                // Bed Wars
+                "Bedwars.kills_bedwars", "Bedwars.final_kills_bedwars",
+
+                // Murder Mystery
+                "MurderMystery.kills",
+
+                // Duels
+                "Duels.kills", "Duels.bridge_kills",
+
+                // Pit
+                "Pit.pit_stats_ptl.kills",
+
+                // Wool Wars
+                "WoolGames.wool_wars.stats.kills"
+
+                );
