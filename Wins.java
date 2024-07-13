96308ec5fa4311e3ee94acbdfe3d5ecfe056d0c1
Stuffy <77872467+stuffyerface@users.noreply.github.com>
2024-07-13 08:15:10 -0400

    Create Wins.java

---
+++ Wins.java
@@ -0,0 +1,77 @@
+List<String> winsKeys = Arrays.asList(
+                // Arcade
+                "Arcade.wins_party", "Arcade.wins_soccer", "Arcade.wins_mini_walls", "Arcade.wins_party_2", "Arcade.wins_farm_hunt",
+                "Arcade.wins_ender", "Arcade.wins_dayone", "Arcade.wins_simon_says", "Arcade.wins_oneinthequiver",
+                "Arcade.seeker_wins_hide_and_seek", "Arcade.hider_wins_hide_and_seek", "Arcade.wins_hole_in_the_wall",
+                "Arcade.sw_game_wins", "Arcade.wins_zombies", "Arcade.wins_hypixel_sports", "Arcade.wins_draw_their_thing",
+                "Arcade.wins_throw_out", "Arcade.wins_santa_simulator", "Arcade.wins_dragonwars2", "Arcade.wins_easter_simulator",
+                "Arcade.wins_scuba_simulator", "Arcade.wins_halloween_simulator", "Arcade.wins_grinch_simulator_v2",
+                "Arcade.pixel_party.wins", "Arcade.woolhunt_participated_wins", "Arcade.dropper.wins",
+
+                // Arena Brawl
+                "Arena.wins",
+
+                // Warlords
+                "Battleground.wins",
+
+                // Blitz SG TODO: Double check this
+                "HungerGames.wins", "HungerGames.wins_teams",
+
+                // Cops and Crims TODO: Double check this
+                "MCGO.game_wins", "MCGO.game_wins_deathmatch",
+
+                // Paintball
+                "Paintball.wins",
+
+                // Quakecraft TODO: Double check this
+                "Quake.wins", "Quake.wins_teams", "Quake.wins_solo_tourney", "Quake.wins_tourney_quake_solo2_1",
+
+                // TNT Games TODO: Double check this
+                "TNTGames.wins", "TNTGames.wins_tourney_tnt_run_0", "TNTGames.wins_tourney_tnt_run_1",
+
+                // UHC TODO: Double check this
+                "UHC.wins", "UHC.wins_solo",
+
+                // VampireZ
+                "VampireZ.human_wins", "VampireZ.vampire_wins",
+
+                // Walls
+                "Walls.wins",
+
+                // Mega Walls
+                "Walls3.wins",
+
+                // Turbo Kart Racers TODO: Reduce to golds?
+                "GingerBread.gold_trophy", "GingerBread.tourney_gingerbread_solo_1_gold_trophy",
+
+                // SkyWars
+                "SkyWars.wins",
+
+                // Crazy Walls
+                "TrueCombat.wins",
+
+                // Smash Heroes
+                "SuperSmash.wins",
+
+                // Speed UHC
+                "SpeedUHC.wins",
+
+                // SkyClash
+                "SkyClash.wins",
+
+                // Bed Wars TODO: Double check this
+                "Bedwars.wins_bedwars",
+
+                // Murder Mystery
+                "MurderMystery.wins",
+
+                // Duels
+                "Duels.wins",
+
+                // Build Battle
+                "BuildBattle.wins",
+
+                // Wool Wars
+                "WoolGames.wool_wars.stats.wins"
+
+        );
